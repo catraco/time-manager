f90ebf700a7b79ade96e25e843ca819f22d4bc84
loser <93885462+catraco@users.noreply.github.com>
2023-01-07 19:48:44 +0100

    Add local time events to

--- assets/main.js
+++ assets/main.js
@@ -2,15 +2,20 @@
 
 const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 var SELECTED_DAY = "Monday";
-var local_userdata = {};
+var data_time = {};
+var page_loading = document.querySelector(".container-page-loading");
+var date = document.querySelector(".container-header-date");
+var time = document.querySelector(".container-header-time");
+var aside = document.querySelector(".container-body-aside");
+var table = document.querySelector(".section-table");
 
-// For developer //
+// @ For developer
 function clear_local_userdata(){
 
     for(let id1=0; id1<7; id1++){
         local_userdata[DAYS[id1]] = [];
         for(let id2=0; id2<24; id2++){
-            local_userdata[DAYS[id1]].push("✕");
+            local_userdata[DAYS[id1]].push("");
         }
     }
     localStorage.setItem("local_userdata", JSON.stringify(local_userdata));
@@ -18,15 +23,8 @@ function clear_local_userdata(){
 
 function disable_default_events(){
 
-    // Disable default events //
+    // Disable default events
     document.oncontextmenu = (event) => {
-        event.preventDefault();
-    }
-
-    document.onselect = (event) => {
-        event.preventDefault();
-    }
-    document.onselectionchange = (event) => {
         event.preventDefault();
     }
     document.onselectstart = (event) => {
@@ -37,19 +35,21 @@ function disable_default_events(){
 
 function load_local_userdata(){
 
-    // Check if deosn't local_userdata exist //
-    if(!localStorage.getItem("local_userdata")){
-        for(let id1=0; id1<7; id1++){
-            local_userdata[DAYS[id1]] = [];
-            for(let id2=0; id2<24; id2++){
-                local_userdata[DAYS[id1]].push("✕");
+    if(!localStorage.getItem("local_userdata")){ // Check if local_userdata doesn't exist
+
+        window.local_userdata = {};
+        for(let index_1=0; index_1<7; index_1++){
+            local_userdata[DAYS[index_1]] = [];
+            for(let index_2=0; index_2<24; index_2++){
+                local_userdata[DAYS[index_1]].push("");
             }
         }
         localStorage.setItem("local_userdata", JSON.stringify(local_userdata));
-    }
-    // If local_userdata exist //
-    else{
-        local_userdata = JSON.parse(localStorage.getItem("local_userdata"));
+
+    } else{ // If local_userdata exist
+
+        window.local_userdata = JSON.parse(localStorage.getItem("local_userdata"));
+        
     }
 
 }
@@ -61,23 +61,45 @@ function edit_local_userdata(hour, value){
 
 }
 
+function get_local_date(){
+
+    let DATE = new Date();
+    data_time = {
+        "hour": DATE.getHours(),
+        "day": () => {
+            for(let index=0; index<7; index++){
+                if(DAYS[index].includes(DATE.toDateString().slice(0, 3))){
+                    SELECTED_DAY = DAYS[index];
+                }
+            }
+            return SELECTED_DAY;
+        },
+        "fullTime": DATE.toLocaleTimeString(),
+        "fullDate": DATE.toDateString()
+    }
+
+    // Update header
+    time.innerHTML = data_time["fullTime"];
+    date.innerHTML = data_time["fullDate"];
+
+    // Update current hour
+    document.getElementById(`${data_time["hour"]}`).classList.add("current-hour");
+
+}
+
 function init_aside(){
 
-    // Build aside //
+    // Init aside //
     for(let index=0; index<7; index++){
-        document.querySelector(".container-body-aside").innerHTML += `<div class="days${DAYS[index]==SELECTED_DAY?" selected-day":""}">${DAYS[index]}</div>`;
+        aside.innerHTML += `<div class="days${DAYS[index]==SELECTED_DAY?" selected-day":""}" id="${DAYS[index]}">${DAYS[index]}</div>`;
     }
 
     // Add click event to days //
     Array.from(document.getElementsByClassName("days")).forEach((element)=>{
         element.addEventListener("click", ()=>{
-            Array.from(document.getElementsByClassName("days")).forEach((element)=>{
-                if(element.classList.contains("selected-day")){
-                    element.classList.remove("selected-day");
-                }
-            });
+            document.getElementById(SELECTED_DAY).classList.remove("selected-day");
             element.classList.add("selected-day");
-            SELECTED_DAY = element.innerText;
+            SELECTED_DAY = element.getAttribute("id");
             update_calendar();
         })
     });
@@ -86,25 +108,26 @@ function init_aside(){
 
 function init_calendar(){
 
-    // Build calendar //
+    // Init calendar //
     let sub_hour = "", hour_count = 0;
     for(let index_1=1; index_1<=6; index_1++){
-        document.querySelector(".section-table").innerHTML += "<tr></tr>";
+        table.innerHTML += "<tr></tr>";
         for(let index_2=1; index_2<=4; index_2++){
             sub_hour = hour_count >= 10 ? String(hour_count) : "0" + String(hour_count);
             document.getElementsByTagName("tr")[index_1-1].innerHTML += `\
             <td>\
-                <span class="hours">${sub_hour}:00</span>\
-                <textarea class="textarea" hour="${hour_count}"></textarea>\
+                <span class="hours">${sub_hour} : 00</span>\
+                <textarea class="textarea" id="${hour_count}" spellcheck="false"></textarea>\
             </td>`;
             hour_count++;
         }
     }
 
-    // Add click event to days //
+    // Add change event to textarea //
     Array.from(document.getElementsByClassName("textarea")).forEach((element) => {
         element.addEventListener("change", () => {
-            edit_local_userdata(Number(element.getAttribute("hour")), element.value);
+            edit_local_userdata(Number(element.getAttribute("id")), element.value);
+            update_calendar();
         });
     });
 
@@ -113,6 +136,11 @@ function init_calendar(){
 function update_calendar(){
 
     for(let index=0; index<24; index++){
+        if (!local_userdata[SELECTED_DAY][index]) {
+            document.getElementsByClassName("hours")[index].classList.add("empty-hour");
+        }else if(document.getElementsByClassName("hours")[index].classList.contains("empty-hour")){
+            document.getElementsByClassName("hours")[index].classList.remove("empty-hour");
+        }
         document.getElementsByTagName("textarea")[index].value = local_userdata[SELECTED_DAY][index];
     }
 
@@ -122,13 +150,14 @@ function setup(){
 
     disable_default_events();
     load_local_userdata();
+    setInterval(get_local_date, 1000);
     init_aside();
     init_calendar();
     update_calendar();
 
     // Remove loding page //
     setTimeout(()=>{
-        document.getElementsByClassName("container-page-loading")[0].style.display = "none";
+        page_loading.style.display = "none";
     }, 1000);
 
 }
