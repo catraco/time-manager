cce9f5170eb1a81bd087990d83e0f22c5723abd6
loser <93885462+catraco@users.noreply.github.com>
2023-01-08 12:05:57 +0100

    Organize the code more

--- assets/main.js
+++ assets/main.js
@@ -1,29 +1,19 @@
 // Main Script File //
 
+// Variables & Constants
 const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-var SELECTED_DAY = "Monday";
-var data_time = {};
 var page_loading = document.querySelector(".container-page-loading");
 var date = document.querySelector(".container-header-date");
 var time = document.querySelector(".container-header-time");
 var aside = document.querySelector(".container-body-aside");
 var table = document.querySelector(".section-table");
+var global_data = {};
+var local_userdata = {};
+var data_time = {};
 
-// @ For developer
-function clear_local_userdata(){
-
-    for(let id1=0; id1<7; id1++){
-        local_userdata[DAYS[id1]] = [];
-        for(let id2=0; id2<24; id2++){
-            local_userdata[DAYS[id1]].push("");
-        }
-    }
-    localStorage.setItem("local_userdata", JSON.stringify(local_userdata));
-}
-
+// Disable default events
 function disable_default_events(){
 
-    // Disable default events
     document.oncontextmenu = (event) => {
         event.preventDefault();
     }
@@ -33,11 +23,12 @@ function disable_default_events(){
 
 }
 
+// load local userdata
 function load_local_userdata(){
 
-    if(!localStorage.getItem("local_userdata")){ // Check if local_userdata doesn't exist
+    // check if local_userdata doesn't exist
+    if(!localStorage.getItem("local_userdata")){
 
-        window.local_userdata = {};
         for(let index_1=0; index_1<7; index_1++){
             local_userdata[DAYS[index_1]] = [];
             for(let index_2=0; index_2<24; index_2++){
@@ -46,67 +37,68 @@ function load_local_userdata(){
         }
         localStorage.setItem("local_userdata", JSON.stringify(local_userdata));
 
-    } else{ // If local_userdata exist
+    }
+    // if local_userdata exist
+    else{
 
-        window.local_userdata = JSON.parse(localStorage.getItem("local_userdata"));
+        local_userdata = JSON.parse(localStorage.getItem("local_userdata"));
         
     }
 
 }
 
+// Edit local userdata
 function edit_local_userdata(hour, value){
 
-    local_userdata[SELECTED_DAY][hour] = value;
+    local_userdata[global_data["selected_day"]][hour] = value;
     localStorage.setItem("local_userdata", JSON.stringify(local_userdata));
 
 }
 
 function get_local_date(){
 
-    let DATE = new Date();
+    DATE = new Date();
+
     data_time = {
         "hour": DATE.getHours(),
-        "day": () => {
-            for(let index=0; index<7; index++){
-                if(DAYS[index].includes(DATE.toDateString().slice(0, 3))){
-                    SELECTED_DAY = DAYS[index];
-                }
-            }
-            return SELECTED_DAY;
-        },
+        "day": "",
         "fullTime": DATE.toLocaleTimeString(),
         "fullDate": DATE.toDateString()
     }
 
-    // Update header
-    time.innerHTML = data_time["fullTime"];
-    date.innerHTML = data_time["fullDate"];
-
-    // Update current hour
-    document.getElementById(`${data_time["hour"]}`).classList.add("current-hour");
+    for(let index=0; index<7; index++){
+        if(DAYS[index].includes(DATE.toDateString().slice(0, 3))){
+            data_time["day"] = DAYS[index];
+            break;
+        }
+    }
 
 }
 
-function init_aside(){
+// Init all html element
+function init_html(){
+
+    for(let index=0; index<7; index++){
+        if(DAYS[index].includes(DATE.toDateString().slice(0, 3))){
+            global_data["selected_day"] = DAYS[index];
+            break;
+        }
+    }
 
     // Init aside //
     for(let index=0; index<7; index++){
-        aside.innerHTML += `<div class="days${DAYS[index]==SELECTED_DAY?" selected-day":""}" id="${DAYS[index]}">${DAYS[index]}</div>`;
+        aside.innerHTML += `<div class="days${DAYS[index]==global_data["selected_day"]?" selected-day":""}" id="${DAYS[index]}">${DAYS[index]}</div>`;
     }
 
     // Add click event to days //
     Array.from(document.getElementsByClassName("days")).forEach((element)=>{
         element.addEventListener("click", ()=>{
-            document.getElementById(SELECTED_DAY).classList.remove("selected-day");
+            document.getElementById(global_data["selected_day"]).classList.remove("selected-day");
             element.classList.add("selected-day");
-            SELECTED_DAY = element.getAttribute("id");
+            global_data["selected_day"] = element.getAttribute("id");
             update_calendar();
-        })
+        });
     });
-
-}
-
-function init_calendar(){
 
     // Init calendar //
     let sub_hour = "", hour_count = 0;
@@ -131,37 +123,71 @@ function init_calendar(){
         });
     });
 
+    // Update calendar after init //
+    update_calendar();
+
 }
 
+// update calendar
 function update_calendar(){
 
     for(let index=0; index<24; index++){
-        if (!local_userdata[SELECTED_DAY][index]) {
+        if (!local_userdata[global_data["selected_day"]][index]) {
             document.getElementsByClassName("hours")[index].classList.add("empty-hour");
         }else if(document.getElementsByClassName("hours")[index].classList.contains("empty-hour")){
             document.getElementsByClassName("hours")[index].classList.remove("empty-hour");
         }
-        document.getElementsByTagName("textarea")[index].value = local_userdata[SELECTED_DAY][index];
+        document.getElementsByTagName("textarea")[index].value = local_userdata[global_data["selected_day"]][index];
     }
 
 }
 
-function setup(){
+// Update all html element
+function update(){
 
-    disable_default_events();
-    load_local_userdata();
-    setInterval(get_local_date, 1000);
-    init_aside();
-    init_calendar();
-    update_calendar();
+    // Update header - time & date
+    time.innerHTML = data_time["fullTime"];
+    date.innerHTML = data_time["fullDate"];
 
-    // Remove loding page //
+    // Update calendar - current hour
+    document.getElementById(`${data_time["hour"]}`).classList.add("current-hour");
+
+}
+
+// Remove loading page 
+function loading_success(){
+
     setTimeout(()=>{
         page_loading.style.display = "none";
     }, 1000);
 
 }
 
+// Main loop function
+function run(){
+
+    setInterval(() => {
+
+        get_local_date();
+        update();
+
+    }, 1000);
+
+}
+
+// Setup application
+function setup(){
+
+    disable_default_events();
+    load_local_userdata();
+    get_local_date();
+    init_html();
+    run();
+    loading_success();
+
+}
+
+// On window load
 window.onload = () => {
 
     // Setup application //
